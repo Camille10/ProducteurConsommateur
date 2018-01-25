46536db6a7739a10381213d75346c888b8b72b2c
Camille10 <camill.bertrand@gmail.com>
2018-01-25 11:13:16 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,14 +13,21 @@ namespace ProducteurConsommateur
         static void Main(string[] args)
         {
             QueueToProduceAndSort cqueue = new QueueToProduceAndSort(NbElement);
-            Producteur producteur = new Producteur(cqueue);
-            Consommateur consommateur = new Consommateur(cqueue);
-            producteur.Run();
-            consommateur.Run();
+            SortedListOutput sortedListOutput = new SortedListOutput();
+
+            for (int index=0; index < 2; index++)
+            {
+                Producteur producteur = new Producteur(cqueue);
+                producteur.Run();
+                Consommateur consommateur = new Consommateur(cqueue, sortedListOutput);
+                consommateur.Run();
+            }
             while (!(cqueue.IsFinished && cqueue.IsEmpty))
             {
                 Thread.Sleep(1000);
             }
+            sortedListOutput.DisplaySortedList();
         }
     }
 }
+
