0697928fcb1cef465b68b1547b11808daced3dea
Camille10 <camill.bertrand@gmail.com>
2018-01-25 11:13:54 +0100

    Add files via upload

---
+++ SortedListOutputTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProducteurConsommateur.Tests
+{
+    [TestClass()]
+    public class SortedListOutputTests
+    {
+        public SortedListOutput sortedListClass = new SortedListOutput();
+
+
+        [TestMethod()]
+        public void should_be_add_in_sortedList()
+        {
+            Random randomNumber = new Random();
+            int number = randomNumber.Next();
+            sortedListClass.Add(number, number);
+            Assert.AreEqual(sortedListClass.sortedList.Count, 1);
+        }
+    }
+}
