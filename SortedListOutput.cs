8d1d74e466582fdfa0476a482bbea00ad3dffd4c
Camille10 <camill.bertrand@gmail.com>
2018-01-25 11:12:28 +0100

    Add files via upload

---
+++ SortedListOutput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducteurConsommateur
+{
+    public class SortedListOutput
+    {
+        public SortedList<int, int> sortedList = new SortedList<int, int>();
+
+        public void Add(int key,int value)
+        {
+            Monitor.Enter(this);
+            sortedList.Add(key, value);
+            Monitor.Exit(this);
+        }
+
+        public void DisplaySortedList()
+        {
+            foreach (int number in sortedList.Values)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
