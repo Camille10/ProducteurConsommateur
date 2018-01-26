5cefd051db85e772a332e07a35e6121ecc5d54c2
Camille10 <camill.bertrand@gmail.com>
2018-01-26 12:33:05 +0100

    Update SortedListOutput.cs

--- SortedListOutput.cs
+++ SortedListOutput.cs
@@ -1,21 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ProducteurConsommateur
 {
-    public class SortedListOutput
+    public class OutputList
     {
         public SortedList<int, int> sortedList = new SortedList<int, int>();
 
         public void Add(int key,int value)
         {
             Monitor.Enter(this);
-            sortedList.Add(key, value);
-            Monitor.Exit(this);
+            try
+            {
+                sortedList.Add(key, value);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Duplicate Keys appears. Element not inserted");
+            }
+            finally
+            {
+                Monitor.Exit(this);
+            }
         }
 
         public void DisplaySortedList()
