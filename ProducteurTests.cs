69d21e82c38f927de076b265210cc24c780a9ef2
Camille10 <camill.bertrand@gmail.com>
2018-02-06 12:56:15 +0100

    Update ProducteurTests.cs

--- ProducteurTests.cs
+++ ProducteurTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ProducteurConsommateur.Tests
 {
@@ -6,22 +6,21 @@ namespace ProducteurConsommateur.Tests
     public class ProducteurTests
     {
         Producteur producteur;
-        QueueToProduceAndSort cqueue;
+        CommunicationQueue cqueue;
         private static int nbElement = 1000;
 
         public ProducteurTests()
         {
-            cqueue = new QueueToProduceAndSort(nbElement);
+            cqueue = new CommunicationQueue(nbElement);
             producteur = new Producteur(cqueue);
         }
-    
+
         [TestMethod]
         public void Should_be_fill_the_queue()
         {
-        producteur.Run();
-        while (!cqueue.IsFinished){}
-        Assert.AreEqual(true, cqueue.IsFinished);
+            producteur.Run();
+            while (!cqueue.IsFinished){}
+            Assert.AreEqual(true, cqueue.IsFinished);
         }
     }
-    
 }
